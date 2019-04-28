c887388f68288dbda935e4d84af979876041ccd6
eakmotion <eakpun@gmail.com>
2019-04-28 16:20:38 +0700

    Add footer session and get site links from Contentful API

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.sass"
+import Footer from "./footer";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -21,12 +22,25 @@ const Layout = ({ children }) => (
             title
           }
         }
+        allContentfulLink(sort: { fields: [createdAt], order: ASC }) {
+          edges {
+            node {
+              title
+              url
+              createdAt
+            }
+          }
+        }
       }
     `}
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title}/>
         <main>{children}</main>
+        <Footer data={data.allContentfulLink}>
+          Copyright © 2019 By{' '}
+          <a href='https://github.com/eakmotion' target='_blank'>Eak Zang</a>
+        </Footer>
       </>
     )}
   />
